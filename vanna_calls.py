527f274501684472e823a2b15534ebf28ae4faa5
tajudeen abdulazeez <tabdulazeez99@gmail.com>
2025-07-22 01:40:15 -0700

    add nlp to sql - vanna ai

---
+++ vanna_calls.py
@@ -0,0 +1,74 @@
+import streamlit as st
+from vanna.chromadb import ChromaDB_VectorStore
+from vanna.openai import OpenAI_Chat
+
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    raise ValueError("Please set the OPENAI_API_KEY environment variable.")
+
+
+class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
+    def __init__(self, config=None):
+        ChromaDB_VectorStore.__init__(self, config=config)
+        OpenAI_Chat.__init__(self, config=config)
+
+
+
+@st.cache_resource(ttl=3600)
+def setup_vanna():
+    vn = MyVanna(config={'api_key': OPENAI_API_KEY, 'model': 'gpt-4o'})
+    vn.connect_to_sqlite('dbn-poc-database.db')
+    return vn
+
+@st.cache_data(show_spinner="Generating sample questions ...")
+def generate_questions_cached():
+    vn = setup_vanna()
+    return vn.generate_questions()
+
+
+@st.cache_data(show_spinner="Generating SQL query ...")
+def generate_sql_cached(question: str):
+    vn = setup_vanna()
+    return vn.generate_sql(question=question, allow_llm_to_see_data=True)
+
+@st.cache_data(show_spinner="Checking for valid SQL ...")
+def is_sql_valid_cached(sql: str):
+    vn = setup_vanna()
+    return vn.is_sql_valid(sql=sql)
+
+@st.cache_data(show_spinner="Running SQL query ...")
+def run_sql_cached(sql: str):
+    vn = setup_vanna()
+    return vn.run_sql(sql=sql)
+
+@st.cache_data(show_spinner="Checking if we should generate a chart ...")
+def should_generate_chart_cached(question, sql, df):
+    vn = setup_vanna()
+    return vn.should_generate_chart(df=df)
+
+@st.cache_data(show_spinner="Generating Plotly code ...")
+def generate_plotly_code_cached(question, sql, df):
+    vn = setup_vanna()
+    code = vn.generate_plotly_code(question=question, sql=sql, df=df)
+    return code
+
+
+@st.cache_data(show_spinner="Running Plotly code ...")
+def generate_plot_cached(code, df):
+    vn = setup_vanna()
+    return vn.get_plotly_figure(plotly_code=code, df=df)
+
+
+@st.cache_data(show_spinner="Generating followup questions ...")
+def generate_followup_cached(question, sql, df):
+    vn = setup_vanna()
+    return vn.generate_followup_questions(question=question, sql=sql, df=df)
+
+@st.cache_data(show_spinner="Generating summary ...")
+def generate_summary_cached(question, df):
+    vn = setup_vanna()
+    return vn.generate_summary(question=question, df=df)
