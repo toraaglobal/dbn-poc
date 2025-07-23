4c1d49d2c66b0b10f3d512d95bbb67e82c88c13e
tajudeen abdulazeez <tabdulazeez99@gmail.com>
2025-07-23 12:21:18 -0700

    update vanna

--- MSME_Loan_Analytics.py
+++ MSME_Loan_Analytics.py
@@ -8,9 +8,9 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import numpy as np
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 st.set_page_config(page_title="MSME Loan Dashboard", layout="wide")
